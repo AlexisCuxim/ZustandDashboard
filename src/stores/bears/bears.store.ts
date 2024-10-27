3df84b9999c218342bb7922c09988b5290f3326c
Alexis Cuxim Poot <alexiscuximpoot@Laptop-de-Alexis.local>
2024-10-26 19:52:03 -0500

    bear store initial

---
+++ src/stores/bears/bears.store.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand';
+
+interface BearStore {
+  blackBears: number;
+  polarBears: number;
+  pandaBears: number;
+  
+  increaseBlackBears: (by: number) => void;
+  increasePolarBears: (by: number) => void;
+  increasePandaBears: (by: number) => void;
+}
+
+export const useBearStore = create<BearStore>()((set) => ({
+  blackBears: 10,
+  polarBears: 5,
+  pandaBears: 1,
+
+  increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
+  increasePolarBears: (by: number) => set((state) => ({ polarBears: state.polarBears + by })),
+  increasePandaBears: (by: number) => set((state) => ({ pandaBears: state.pandaBears + by })),
+}));
