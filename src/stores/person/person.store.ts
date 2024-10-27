c545a025fdd1a1cd7e238e8ca56cc56db335c86b
Alexis Cuxim Poot <alexiscuximpoot@Laptop-de-Alexis.local>
2024-10-27 14:39:39 -0500

    session storage implementation

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,11 +1,12 @@
 import { StateCreator, create } from "zustand";
-import { StateStorage, createJSONStorage, persist } from "zustand/middleware";
+import { persist } from "zustand/middleware";
+import { customSessionStorage } from "../storages/session-storage.storage";
 
 interface PersonState {
   firstName: string;
   lastName: string;
 }
-  
+
 interface Actions {
   setFirstName: (value: string) => void;
   setLastName: (value: string) => void;
@@ -18,21 +19,6 @@ const storeApi: StateCreator<PersonState & Actions> = (set) => ({
   setLastName: (value: string) => set((store) => ({ lastName: value })),
 });
 
-const sessionStorage: StateStorage = {
-  getItem: function (name: string): string | Promise<string | null> | null {
-    console.log('getItem', name);
-    return null;
-  },
-  setItem: function (name: string, value: string): unknown {
-    console.log('setItem', { name, value });
-    return null;
-  },
-  removeItem: function (name: string): unknown {
-    console.log('removeItem', name);
-    return null;
-  }
-}
-
 // Persist funciona como middleware y recibe dos argumentos el primero
 // es el objeto Store y un objeto con el nombre que se guardara en el 
 // localStorage
@@ -41,7 +27,7 @@ export const usePersonStore = create<PersonState & Actions>()(
     storeApi,
     {
       name: 'personStorage',
-      storage: createJSONStorage(() => sessionStorage),
+      storage: customSessionStorage,
     }
   )
 );
