195f9386fa47e24ff73654c848447a0123083e6c
AlexisCuxim <acuximpoot@gmail.com>
2024-11-25 20:39:36 -0500

    change of classes for the selected container

--- src/components/jira/JiraTasks.tsx
+++ src/components/jira/JiraTasks.tsx
@@ -1,4 +1,4 @@
-import { DragEvent } from 'react';
+import { DragEvent, useState } from 'react';
 import { IoCheckmarkCircleOutline, IoEllipsisHorizontalOutline, IoReorderTwoOutline } from 'react-icons/io5';
 import classNames from 'classnames';
 import { Task, TaskStatus } from '../../interfaces';
@@ -14,20 +14,21 @@ interface Props {
 
 export const JiraTasks = ({ title, value, tasks }: Props) => {
   const isDragging = useTaskStore((state) => !!state.draggingTaskId);
+  const [onDragOver, setOnDragOver] = useState(false);
 
   const handleDragOver = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    console.log('onDragOver')
+    setOnDragOver(true);
   };
 
   const handleDragLeave = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    console.log('handleDragLeave')
+    setOnDragOver(false);
   };
 
   const handleDrop = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
-    console.log('handleDrop', value)
+    setOnDragOver(false);
   };
 
   return (
@@ -37,7 +38,8 @@ export const JiraTasks = ({ title, value, tasks }: Props) => {
       onDrop={handleDrop}
       className={
         classNames("!text-black border-4 relative flex flex-col rounded-[20px]  bg-white bg-clip-border shadow-3xl shadow-shadow-500  w-full !p-4 3xl:p-![18px]", {
-          "border-blue-500 border-dotted": isDragging
+          "border-blue-500 border-dotted": isDragging,
+          "border-green-500 border-dotted": isDragging && onDragOver,
         })
       }>
 
