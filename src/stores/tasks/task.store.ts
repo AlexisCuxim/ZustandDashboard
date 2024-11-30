001b2e343c4187131ea49e17622ec8798b787825
AlexisCuxim <acuximpoot@gmail.com>
2024-11-30 11:51:40 -0500

    method produce of immer

--- src/stores/tasks/task.store.ts
+++ src/stores/tasks/task.store.ts
@@ -2,6 +2,7 @@ import { StateCreator, create } from "zustand";
 import type { Task, TaskStatus } from "../../interfaces";
 import { devtools } from "zustand/middleware";
 import { v4 as uuidv4 } from "uuid";
+import { produce } from "immer";
 
 interface TaskState {
   draggingTaskId?: string;
@@ -31,12 +32,16 @@ const storeApi: StateCreator<TaskState> = (set, get) => ({
   addTask: (title: string, status: TaskStatus) => {
     const newTask = { id: uuidv4(), title, status };
 
-    set(state => ({
-      tasks: {
-        ...state.tasks,
-        [newTask.id]: newTask,
-      },
-    }));
+    set(produce((state: TaskState) => {
+      state.tasks[newTask.id] = newTask;
+    }))
+
+    // set(state => ({
+    //   tasks: {
+    //     ...state.tasks,
+    //     [newTask.id]: newTask,
+    //   },
+    // }));
   },
   setDraggingTaskId: (taskId: string) => {
     set({ draggingTaskId: taskId });
